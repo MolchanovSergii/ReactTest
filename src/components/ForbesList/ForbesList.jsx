2cb9e3c30bc906635d1b46b505b3bb7fe6a7169f
Kateryna Artemieva <moruga.katya@gmail.com>
2023-05-27 13:59:06 +0100

    start task-3

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,4 +1,6 @@
 import { ForbesListItem } from 'components';
+import forbes from "data/forbes.json"
+
 import {
   BoardHeader,
   LeaderBoard,
@@ -8,6 +10,19 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({list}) => {
+  return (<LeaderBoard>
+    <BoardHeader>
+      <BoardTitle>
+        <TitleTop>Forbes</TitleTop>
+        <TitleBottom>Leader board</TitleBottom>
+      </BoardTitle>
+    </BoardHeader>
+  
+    <LeaderBoardProfiles>
+      {list.map(({id, name, capital, avatar, isIncrease})=>{
+        <ForbesListItem key={id} name={name} capital={capital} avatar={avatar} isIncrease={isIncrease}/>
+      })}
+    </LeaderBoardProfiles>
+  </LeaderBoard>);
 };
