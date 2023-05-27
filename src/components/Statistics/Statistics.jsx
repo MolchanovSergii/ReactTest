527106b2b6ceb9b3451bdf9330cd8f5727fb16cc
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-05-27 15:28:23 +0300

    task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,30 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp size={30} />,
+  <MdPeople size={30} />,
+  <MdOutlineProductionQuantityLimits size={30} />,
+  <GiTreeDoor size={30} />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+
+      <StatisticsList>
+        {stats.map(({ id, title, total }, idx) => {
+          return (
+            <StatisticItem
+              key={id}
+              icon={icons[idx]}
+              title={title}
+              total={total}
+            />
+          );
+        })}
+      </StatisticsList>
     </>
   );
 };
